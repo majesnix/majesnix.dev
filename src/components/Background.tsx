c350b2bdb2f901504a47dd651129153ec3846d95
majesnix <dominic@dominic-classen.de>
2021-05-28 23:08:32 +0200

    fix: image position (and background css)

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,20 +1,15 @@
 // @ts-ignore
-import image from "../assets/images/yui.jpg";
+import image from "../assets/images/yui.png";
 import styled from "styled-components";
 
 const Background = styled.div`
   height: 380px;
-  position: relative;
-  background-position: center center;
+  background-position-x: 50%;
+  background-position-y: 60%;
+  background-repeat: no-repeat;
   background-size: cover;
-  margin: 0;
-  padding: 0;
-  border: 0;
-  display: flex;
-  align-items: center;
-  background-position: 0px -400px;
-  width: 100%;
   background-image: url(${image});
+  }
 `;
 
 export default Background;
